b1e90f082ba188a4a66844d6b18155787cf0f153
DewaAdyatma <dewaadyatma28@gmail.com>
2024-08-19 13:36:46 +0700

    first commit

---
+++ proyek-akhir/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+function ContainerOutsideExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+    </Navbar>
+  );
+}
+
+export default ContainerOutsideExample;
